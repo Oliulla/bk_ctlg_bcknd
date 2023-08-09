23a3f3ae324a75627e18670b300ed61cbcb76a15
Oliulla <razasm29@gmail.com>
2023-08-09 13:45:43 +0600

    init setup done

---
+++ src/app.ts
@@ -0,0 +1,45 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+import cors from "cors";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import cookieParser from "cookie-parser";
+
+import routes from "./app/routes";
+import httpStatus from "http-status";
+const app: Application = express();
+
+app.use(cors());
+app.use(cookieParser());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Application routes
+// app.use("/api/v1/users", UseRoutes);
+// app.use("/api/v1/academic-semesters", AcademicSemesterRoutes);
+app.use("/api/v1", routes);
+
+// testing
+// app.get("/", async (req: Request, res: Response, next: NextFunction) => {
+//   throw new Error('new logger')
+// })
+
+// global error handler
+app.use(globalErrorHandler);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "Not Found",
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: "Api not found",
+      },
+    ],
+  });
+
+  next();
+});
+
+export default app;
---
+++ src/app/routes/index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+
+const router = express.Router();
+
+const moduleRoutes = [];
+
+moduleRoutes.forEach(route => router.use(route.path, route.route));
+export default router;
